e1574267eca553e1b5cf21b82b2b24e97c2f8766
Milla22 <marti.ludmilla@orange.fr>
2023-10-27 21:01:52 +0200

    impléméntation HTML page photographe+ formulaire

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,13 @@
 //Mettre le code JavaScript lié à la page photographer.html
+const button = document.getElementById('toggleCollapse');
+const content = document.getElementById('collapsibleContent');
+const chevron = document.querySelector('.fa-chevron-down');
+button.addEventListener('click', function() {
+    if (content.style.display === 'none' || content.style.display === '') {
+        content.style.display = 'block';
+        chevron.style.rotate="180deg"
+    } else {
+        content.style.display = 'none';
+        chevron.style.rotate="360deg";
+    }
+});
