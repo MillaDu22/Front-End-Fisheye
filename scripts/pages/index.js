94739c796dd98b985771aa796a35914ee816b9dd
Milla22 <marti.ludmilla@orange.fr>
2023-10-31 22:23:29 +0100

    page accueil dynamique

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -44,5 +44,37 @@ async function init() {
 
 init();*/
 
+/*************************************** Fetch datas photographers HomePage***********************************/
+const urlDatas = ("../../data/photographers.json");
+const sectionPhotographers = document.getElementById("section-photographers");
+
+const getPhotographers = () => {
+    fetch(urlDatas)
+    .then(function (response) {
+        return response.json()
+    })
+    .then(function (data){
+        console.log(data)
+        const photographers = data.photographers;
+        photographers.forEach(function (photographer) {
+            const imagePath = `../../assets/images/photographers/${photographer.portrait}`;
+            sectionPhotographers.innerHTML += `
+            <article class="photographer-container">
+                <a href="./photographer.html" class="link-focus" aria-label="cette carte est un lien vers la page de ${photographer.name}" tabindex="0">
+                    <span class="container-img">
+                        <img src="${imagePath}" class="photographer-photography" alt="portrait du photographe">
+                    </span>
+                    <h2 class="name">${photographer.name}</h2>
+                </a>
+                <h3 class="location">${photographer.city}, ${photographer.country}</h3>
+                <p class="title-photographer">${photographer.tagline}</p>
+                <p class="price">${photographer.price}€/jour</p>
+            </article>`;
+        });
+    })
+}
+getPhotographers()
+
+
 
 
