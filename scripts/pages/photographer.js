48f38d17377f34ddb74f1577d30dea9626ba9e27
Milla22 <marti.ludmilla@orange.fr>
2023-11-06 17:26:41 +0100

    filrage avec methode sort

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -38,23 +38,49 @@ document.addEventListener("DOMContentLoaded", function () {
 getPhotographerById(photographerId);
 });
 
-//////////////////////////////////////////////////////fetch pour gallery medias////////////////////////////////////////
-const galleryContainer = document.getElementById('gallery-photografer');
+//////////////////////////////////////////////////////fetch pour gallery medias + select////////////////////////////////////////
 
-function loadPhotographerMedia(photographerId) {
-    fetch('../../data/photographers.json')
-    .then((response) => response.json())
-    .then((data) => {
+const galleryContainer = document.getElementById('gallery-photografer');
+const dropdownWrapper = document.querySelector('#dropdown-wrapper');
+const links = document.querySelectorAll('.dropdown-list a');
+const span = document.querySelector('.selected');
+let photographerMedia = [];
+
+// Recherche datas photographerID //
+async function loadPhotographerData(photographerId) {
+    try {
+        const response = await fetch('../../data/photographers.json');
+        const data = await response.json();
         const photographers = data.photographers;
-        const photographer = photographers.find(photographer => photographer.id === parseInt(photographerId));
-        const photographerMedia = data.media.filter((media) => media.photographerId == photographer.id);
-        photographerMedia.forEach((media, index) => {
-            if (media.video) {
-                galleryContainer.innerHTML += `
+        const photographer = photographers.find((photographer) => photographer.id === parseInt(photographerId));
+        photographerMedia = data.media.filter((media) => media.photographerId == photographer.id);
+    } catch (error) {
+        console.error('Erreur au chargement des données du photographe :', error);
+    }
+}
+// Tri avec méthode sort //
+function loadSortedPhotographerMedia(option) {
+    let sortedMedia = [...photographerMedia];
+    if (option === 'Popularité') {
+        sortedMedia.sort((a, b) => b.likes - a.likes);
+        // Tri du plus populaire au moins populaire //
+    } else if (option === 'Date') {
+        sortedMedia.sort((a, b) => new Date(b.date) - new Date(a.date));
+        // Tri du plus récent au plus ancien//
+    } else if (option === 'Titre') {
+        sortedMedia.sort((a, b) => a.title.localeCompare(b.title));
+        // Tri par ordre alphabétique avec la méthode localeCompare qui détermine la façon dont les caractères sont triés //
+    }
+    // Pour chaque media le HTML suivant//
+    galleryContainer.innerHTML = '';
+    sortedMedia.forEach((media, index) => {
+        // Si c'est une video//
+        if (media.video) {
+            galleryContainer.innerHTML += `
                 <div class="element-gallery">
                     <div class="box-video active">
                         <video class="video-gallery" controls>
-                            <source src="../../assets/images/gallery-id/${photographer.id}/${media.video}" type="video/mp4">
+                            <source src="../../assets/images/gallery-id/${photographerId}/${media.video}" type="video/mp4">
                             Votre navigateur ne supporte pas la lecture de la vidéo.
                         </video>
                     </div>
@@ -63,62 +89,61 @@ function loadPhotographerMedia(photographerId) {
                         <span class="like">${media.likes}<i class="fa-solid fa-heart" aria-hidden="true"></i></span>
                     </div>
                 </div>`;
-            } else if (media.image) {
-                galleryContainer.innerHTML += `
+        //Si c'est une image//        
+        } else if (media.image) {
+            galleryContainer.innerHTML += `
                 <div class="element-gallery">
-                    <div class="box-image active">
-                        <img src="../../assets/images/gallery-id/${photographer.id}/${media.image}" class="img-gallery" alt="${media.title}" onclick="openModal();currentSlide(${index + 1})">
+                    <div class "box-image active">
+                        <img src="../../assets/images/gallery-id/${photographerId}/${media.image}" class="img-gallery" alt="${media.title}" onclick="openModal();currentSlide(${index + 1})">
                     </div>
                     <div class="title-img">
                         <h4 class="txt">${media.title}</h4>
                         <span class="like">${media.likes}<i class="fa-solid fa-heart" aria-hidden="true"></i></span>
                     </div>
                 </div>`;
-            }
-        });
-    })
-    .catch((error) => {
-        console.error('Erreur au chargement des médias :', error);
+        }
     });
 }
 
 function getPhotographerIdFromURL() {
     const urlParams = new URLSearchParams(window.location.search);
     const idParam = urlParams.get('id');
-    
+
     if (idParam) {
-        // Si le paramètre 'id' existe dans l'URL, ça retourne sa valeur sous forme de nombre
         return parseInt(idParam);
     } else {
-        // Si le paramètre 'id' n'existe pas ou n'est pas un nombre valide, ça retourne null
         return null;
     }
 }
 
+// Si photographerID trouvé média affichés par popularité par défaut //
 const photographerId = getPhotographerIdFromURL();
 if (photographerId !== null) {
     console.log('ID du photographe depuis URL :', photographerId);
+    loadPhotographerData(photographerId)
+        .then(() => {
+            loadSortedPhotographerMedia('Popularité'); // Affichage par popularité par défaut
+        })
+        .catch((error) => {
+            console.error('Erreur au chargement des médias du photographe :', error);
+        });
 } else {
     console.log('ID non trouvé ou invalide.');
 }
-loadPhotographerMedia(photographerId);
 
-
-
-/////////////////////////////////////////////////////// Select ////////////////////////////////////////////////////////////
-const dd = document.querySelector('#dropdown-wrapper');
-const links = document.querySelectorAll('.dropdown-list a');
-const span = document.querySelector('.selected');
-
-dd.addEventListener('click', function() {
+// Gestionnaire d'événements pour le changement d'option de tri
+dropdownWrapper.addEventListener('click', function () {
     this.classList.toggle('is-active');
 });
 
 links.forEach((element) => {
-    element.addEventListener('click', function(evt) {
-    span.innerHTML = evt.currentTarget.textContent;
-    })
-})
+    element.addEventListener('click', function (evt) {
+        span.innerHTML = evt.currentTarget.textContent;
+        loadSortedPhotographerMedia(evt.currentTarget.textContent);
+    });
+});
+
+
 ////////////////////////////////////////////////////////Formulaire de contact///////////////////////////////////////////////////
 // Fonction open modal
 function openModalForm() {
@@ -133,9 +158,6 @@ function closeModal() {
     });
 }
 closeModal();
-
-// Événement au clic pour ouvrir la modal
-document.getElementById("open-modal-button").addEventListener("click", openModalForm);
 
 /////////////////////////////////////////////////////////// LightBox /////////////////////////////////////////////////////////
 const slides = document.querySelector('.lightbox-image');
