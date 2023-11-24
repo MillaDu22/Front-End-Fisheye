5b4de1251ba95434fd2f46f3d6e6624e26f0462a
Milla22 <marti.ludmilla@orange.fr>
2023-11-24 17:28:58 +0100

    indentation

--- scripts/utils/lightbox.js
+++ scripts/utils/lightbox.js
@@ -38,10 +38,10 @@ export const displayLightboxMedias = medias => {
         const currentSlide = listOfMedias[currentIndex];
         boxMedia.innerHTML=`
         ${currentSlide._image ? 
-            `<img class ="media-lightbox" src = "../../assets/images/gallery-id/${currentSlide._photographerId}/${currentSlide._image}"
+            `<img class ="media-lightbox" src = "./assets/images/gallery-id/${currentSlide._photographerId}/${currentSlide._image}"
                 alt= "${currentSlide._alt}">` :
             `<video class="media-lightbox" controls  aria-label ="${currentSlide._title}">
-                <source src ="../../assets/images/gallery-id/${currentSlide._photographerId}/${currentSlide._video}" type="video/mp4">
+                <source src ="./assets/images/gallery-id/${currentSlide._photographerId}/${currentSlide._video}" type="video/mp4">
             </video>`}
             <p class ="title-slide">${currentSlide._title}</p>`;
     };
